35c57f0daa6b8435fb947eaa59fd59ce0b97b036
Tony Bao <tony@pathlight.com>
2022-09-28 14:56:16 -0400

    clean file

--- tap_lessonly/__init__.py
+++ tap_lessonly/__init__.py
@@ -34,8 +34,6 @@ def discover():
         key_properties = ['id']
 
         replication_key = None
-        if stream_id == 'qa':
-            replication_key = 'sequence_id'
 
         stream_metadata = metadata.get_standard_metadata(
             schema=schema.to_dict(),
@@ -60,43 +58,6 @@ def discover():
             )
         )
     return Catalog(streams)
-
-
-# def sync(config, state, catalog):
-#     """ Sync data from tap source """
-#     # Loop over selected streams in catalog
-#     for stream in catalog.get_selected_streams(state):
-#         LOGGER.info("Syncing stream:" + stream.tap_stream_id)
-
-#         bookmark_column = stream.replication_key
-#         is_sorted = True  # TODO: indicate whether data is sorted ascending on bookmark value
-
-#         singer.write_schema(
-#             stream_name=stream.tap_stream_id,
-#             schema=stream.schema,
-#             key_properties=stream.key_properties,
-#         )
-
-#         # TODO: delete and replace this inline function with your own data retrieval process:
-#         tap_data = lambda: [{"id": x, "name": "row${x}"} for x in range(1000)]
-
-#         max_bookmark = None
-#         for row in tap_data():
-#             # TODO: place type conversions or transformations here
-
-#             # write one or more rows to the stream:
-#             singer.write_records(stream.tap_stream_id, [row])
-#             if bookmark_column:
-#                 if is_sorted:
-#                     # update bookmark to latest value
-#                     singer.write_state({stream.tap_stream_id: row[bookmark_column]})
-#                 else:
-#                     # if data unsorted, save max value until end of writes
-#                     max_bookmark = max(max_bookmark, row[bookmark_column])
-#         if bookmark_column and not is_sorted:
-#             singer.write_state({stream.tap_stream_id: max_bookmark})
-#     return
-
 
 @utils.handle_top_exception(LOGGER)
 def main():
